7d64df2b157c53665b5d3dfa28de7cc209f5cb71
MyKroft <petermichael908@gmail.com>
2018-08-27 22:41:01 +0100

    first

--- index.js
+++ index.js
@@ -12,10 +12,30 @@ const ffmpeg = require('fluent-ffmpeg');
 app.use(compression());
 
 app.get('/', function(req, res) {
-    
-        res.send("results");
-    
+    var opts = {
+        maxResults: 10,
+        key: config.YT_KEY
+      };
+       
+      search('nwa baby', opts, function(err, results) {
+        if(err) return console.log(err);
+        let stream = ytdl(results[0].link, {
+            quality: 'highestaudio',
+            //filter: 'audioonly',
+          });
+          ffmpeg(stream)
+          .audioBitrate(128)
+          .save(`nwa.mp3`)
+          .on('progress', (p) => {
+            readline.cursorTo(process.stdout, 0);
+            process.stdout.write(`${p.targetSize}kb downloaded`);
+          })
+          .on('end', () => {
+            console.log(`\ndone, thanks - ${(Date.now() - start) / 1000}s`);
+        });
+        res.send(results);
+      });
       
 });
 
-app.listen(port)
+app.listen(port,"127.0.0.1");
