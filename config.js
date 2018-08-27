3cfffe46297b2e82ecf09b9ed501fc41ea6ce835
MyKroft <petermichael908@gmail.com>
2018-08-27 20:25:54 +0100

    first

---
+++ config.js
@@ -0,0 +1,23 @@
+var config = {
+    PORT: '80',
+    UN_VERIFIED: "unverified",
+    VERIFIED: "verified",
+    YES: "yes",
+    NO: "no",
+    TRUE: "true",
+    FALSE: "false",
+    DONE: "done",
+    PLANNER: "planner",
+    COUPLE: "couple",
+    DELETE: "delete",
+    EDIT: "edit",
+    SET_REMINDER: "set reminder",
+    ERROR: "error",
+    VALID: "valid",
+    SENT: "sent",
+    TIMER_DEFAULT: 120,
+    VIEW_REMINDER: 'View Reminder',
+    YT_KEY: "AIzaSyDzi_k3H_e7OtktXLidaxUzzSZ_Xd8t8l4"
+};
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var config = require('./config');
+var compression = require('compression');
+var port = process.env.PORT || config.PORT;
+var app = express();
+var fs = require('fs');
+var search = require('youtube-search');
+const ytdl = require('ytdl-core');
+const readline = require('linebyline');
+const ffmpeg = require('fluent-ffmpeg');
+
+app.use(compression());
+
+app.get('/', function(req, res) {
+    var opts = {
+        maxResults: 10,
+        key: config.YT_KEY
+      };
+       
+      search('nwa baby', opts, function(err, results) {
+        if(err) return console.log(err);
+        let stream = ytdl(results[0].link, {
+            quality: 'highestaudio',
+            //filter: 'audioonly',
+          });
+          ffmpeg(stream)
+          .audioBitrate(128)
+          .save(`nwa.mp3`)
+          .on('progress', (p) => {
+            readline.cursorTo(process.stdout, 0);
+            process.stdout.write(`${p.targetSize}kb downloaded`);
+          })
+          .on('end', () => {
+            console.log(`\ndone, thanks - ${(Date.now() - start) / 1000}s`);
+        });
+        res.send(results);
+      });
+      
+});
+
+app.listen(port)
