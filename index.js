e2a5fe82f72838fbdfaddd5623db6431bc658560
MyKroft <petermichael908@gmail.com>
2018-09-01 18:12:02 +0100

    l

--- index.js
+++ index.js
@@ -50,9 +50,9 @@ app.get('/download-song', function(req, res) {
      //process.stdout.write(`${p.targetSize}kb downloaded`);
     })
     .on('end', () => {
-     res.sendFile(name, function(err) {
+     res.sendFile("./"+ name, function(err) {
       if(!err) {
-        fs.unlinkSync(name);
+        fs.unlinkSync("./"+name);
       }
      });
   });
